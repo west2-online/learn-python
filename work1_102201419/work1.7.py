54129dbd08b757c99f7718a899ffaf43fb679209
Sonorry <951452955@qq.com>
2023-10-12 19:37:28 +0800

    work1.3 finished

--- work1_102201419/work1.7.py
+++ work1_102201419/work1.7.py
@@ -1,6 +1,5 @@
 """设计⼀个商品类，它具有的私有数据成员是商品序号、商品名、单价、总数量和剩余数量。具有的 公有成员函数是：初始化商品信息的构造函数__init__，
 显示商品信息的函数display，计算已售出 商品价值income，修改商品信息的函数setdata"""
-
 class Commodity:
     def __init__(self,id,name,price,sum,remain):
         self.__product_id = id
